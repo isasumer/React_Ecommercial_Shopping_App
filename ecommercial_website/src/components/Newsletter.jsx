a8e8f08f082689e1b1e64b80ad5927cf8ed62140
Isa SUMER <sumerisa5@gmail.com>
2021-09-09 20:43:39 +0300

    newsletter finished

--- ecommercial_website/src/components/Newsletter.jsx
+++ ecommercial_website/src/components/Newsletter.jsx
@@ -2,18 +2,43 @@ import { Send } from "@material-ui/icons";
 import styled from "styled-components";
 
 const Container = styled.div`
-height: 60vh;
-background-color:#fcf5f5;
-display:flex;
-align-items:center;
-justify-content: center;
-
+  height: 60vh;
+  background-color: #fcf5f5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `;
-const Title = styled.h1``;
-const Desc = styled.div``;
-const InputContainer = styled.div``;
-const Input = styled.input``;
-const Button = styled.button``;
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+const InputContainer = styled.div`
+width:50%;
+height:40px;
+display:flex;
+justify-content: center;
+align-items:center;
+border:1px solid lightgray;
+`;
+const Input = styled.input`
+flex:8;
+border:none;
+height:100%;
+padding-left: 20px;
+`;
+const Button = styled.button`
+flex:1;
+border:none;
+height:100%;
+background-color:teal ;
+color:white;
+cursor:pointer;
+`;
 
 const Newsletter = () => {
   return (
