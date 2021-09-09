0ee02671f4c73c40de334eca77d8e24e2ca61aef
Isa SUMER <sumerisa5@gmail.com>
2021-09-09 18:29:06 +0300

    adjusted

---
+++ ecommercial_website/src/components/Newsletter.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+
+`;
+
+import { Container } from "@material-ui/core";
+
+const Newsletter = () => {
+    return (
+        <Container></Container>
+    )
+}
+
+export default Newsletter;
