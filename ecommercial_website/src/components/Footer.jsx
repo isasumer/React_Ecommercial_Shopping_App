b6c04c85481f86f2facad2ec6d79828da51a019a
Isa SUMER <sumerisa5@gmail.com>
2021-09-09 21:01:29 +0300

    footer created

--- ecommercial_website/src/components/Footer.jsx
+++ ecommercial_website/src/components/Footer.jsx
@@ -1,10 +1,36 @@
 import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
 
 const Footer = () => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <Container>
+      <Logo>SUMER.</Logo>
+      <Desc>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos
+        quod sequi nesciunt modi debitis esse eos ut molestias, reiciendis
+        repellendus?
+      </Desc>
+      <SocialContainer>
+          <SocialIcon>
+              
+          </SocialIcon>
+      </SocialContainer>
+      <Center></Center>
+      <Right></Right>
+    </Container>
+  );
+};
 
 export default Footer;
