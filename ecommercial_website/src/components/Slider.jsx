bc9c92792ad20819b00b41144441ef83b270e635
Isa SUMER <sumerisa5@gmail.com>
2021-09-09 18:30:00 +0300

    slider redesigned

--- ecommercial_website/src/components/Slider.jsx
+++ ecommercial_website/src/components/Slider.jsx
@@ -95,7 +95,7 @@ const Slider = () => {
       </Arrow>
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => (
-          <Slide bg={item.bg}>
+          <Slide bg={item.bg} key={item.id}>
             <ImgContainer>
               <Image src={item.img}></Image>
             </ImgContainer>
