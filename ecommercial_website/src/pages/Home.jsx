e10a790d259db1be850a82d61f3ff8fc0203d469
Isa SUMER <sumerisa5@gmail.com>
2021-09-09 18:30:14 +0300

    home style changed

--- ecommercial_website/src/pages/Home.jsx
+++ ecommercial_website/src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Announcement from "../components/Announcement"
-import Slider from "../components/Slider"
-import Categories from '../components/Categories'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
+import Categories from '../components/Categories';
+import Products from '../components/Products';
 
 const Home = () => {
     return (
@@ -11,9 +12,10 @@ const Home = () => {
         <Navbar/>
         <Slider/>
         <Categories/>
+        <Products/>
     </div>
         
     )
 }
 
-export default Home
+export default Home;
