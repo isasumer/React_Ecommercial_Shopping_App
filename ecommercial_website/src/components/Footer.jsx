06565ccdd39049a78eb84500683145de9b015bdd
Isa SUMER <sumerisa5@gmail.com>
2021-09-09 21:15:16 +0300

    footer created

--- ecommercial_website/src/components/Footer.jsx
+++ ecommercial_website/src/components/Footer.jsx
@@ -1,10 +1,39 @@
+import {
+  Facebook,
+  Instagram,
+  LinkedIn,
+  Pinterest,
+  Twitter,
+} from "@material-ui/icons";
 import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
+  flex-direction: row;
 `;
+
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: row;
+`;
+const Logo = styled.div`
+  flex: 1;
+
+`;
+const Desc = styled.div`
+  flex: 5;
+
+`;
+const SocialContainer = styled.div`
+  flex: 2;
+
+   display: flex;
+  flex-direction: row;
+`;
+
+const SocialIcon = styled.div`
+  display: flex;
 `;
 const Right = styled.div`
   flex: 1;
@@ -16,17 +45,31 @@ const Center = styled.div`
 const Footer = () => {
   return (
     <Container>
-      <Logo>SUMER.</Logo>
-      <Desc>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos
-        quod sequi nesciunt modi debitis esse eos ut molestias, reiciendis
-        repellendus?
-      </Desc>
-      <SocialContainer>
+      <Left>
+        <Logo>SUMER.</Logo>
+        <Desc>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos
+          quod sequi nesciunt modi debitis esse eos ut molestias, reiciendis
+          repellendus?
+        </Desc>
+        <SocialContainer>
           <SocialIcon>
-              
+            <Facebook />
           </SocialIcon>
-      </SocialContainer>
+          <SocialIcon>
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon>
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon>
+            <Pinterest />
+          </SocialIcon>
+          <SocialIcon>
+            <LinkedIn />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
       <Center></Center>
       <Right></Right>
     </Container>
