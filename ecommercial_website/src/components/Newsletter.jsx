76dd82be3a685d7aed46176afb29f08010a2c680
Isa SUMER <sumerisa5@gmail.com>
2021-09-09 18:49:49 +0300

    newsletter created

--- ecommercial_website/src/components/Newsletter.jsx
+++ ecommercial_website/src/components/Newsletter.jsx
@@ -1,15 +1,33 @@
+import { Send } from "@material-ui/icons";
 import styled from "styled-components";
 
 const Container = styled.div`
+height: 60vh;
+background-color:#fcf5f5;
+display:flex;
+align-items:center;
+justify-content: center;
 
 `;
-
-import { Container } from "@material-ui/core";
+const Title = styled.h1``;
+const Desc = styled.div``;
+const InputContainer = styled.div``;
+const Input = styled.input``;
+const Button = styled.button``;
 
 const Newsletter = () => {
-    return (
-        <Container></Container>
-    )
-}
+  return (
+    <Container>
+      <Title>NewsLetter</Title>
+      <Desc>Lorem ipsum dolor sit amet.</Desc>
+      <InputContainer>
+        <Input placeholder="Enter your email"></Input>
+        <Button>
+          <Send />
+        </Button>
+      </InputContainer>
+    </Container>
+  );
+};
 
 export default Newsletter;
