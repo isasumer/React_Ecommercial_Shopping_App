d11a9f09c1934f2243d2607a33f420a344037b42
Isa SUMER <sumerisa5@gmail.com>
2021-09-09 18:49:31 +0300

    Newslertter added

--- ecommercial_website/src/pages/Home.jsx
+++ ecommercial_website/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
 import Categories from '../components/Categories';
 import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
         <Slider/>
         <Categories/>
         <Products/>
+        <Newsletter/>
     </div>
         
     )
