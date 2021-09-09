89b6ff8c58079d686da302c5e1b26c77ac53dcec
Isa SUMER <sumerisa5@gmail.com>
2021-09-09 21:15:33 +0300

    footer added home

--- ecommercial_website/src/pages/Home.jsx
+++ ecommercial_website/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Slider from "../components/Slider";
 import Categories from '../components/Categories';
 import Products from '../components/Products';
 import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
@@ -15,6 +16,7 @@ const Home = () => {
         <Categories/>
         <Products/>
         <Newsletter/>
+        <Footer/>
     </div>
         
     )
