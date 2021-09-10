499cbfc4ad13b914d696f837a172ab1099dcc692
Isa SUMER <sumerisa5@gmail.com>
2021-09-10 22:58:57 +0300

    app upgraded

--- ecommercial_website/src/App.js
+++ ecommercial_website/src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Home from './pages/Home'
 import ProductList from './pages/ProductList'
 import Product from './pages/Product'
+import Register from './pages/Register'
 
 const App = () => {
   return (
-    <Product/>
+    <Register/>
   )
 }
 
