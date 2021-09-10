8a22abfcf30685f214c875c441d6fb4d7ffbf28f
Isa SUMER <sumerisa5@gmail.com>
2021-09-10 23:11:04 +0300

    app js updated

--- ecommercial_website/src/App.js
+++ ecommercial_website/src/App.js
@@ -3,10 +3,11 @@ import Home from './pages/Home'
 import ProductList from './pages/ProductList'
 import Product from './pages/Product'
 import Register from './pages/Register'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <Register/>
+    <Login/>
   )
 }
 
