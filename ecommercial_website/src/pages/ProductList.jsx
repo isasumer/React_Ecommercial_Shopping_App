83d202549b30e54e67ca5d6ce23a72423927be06
Isa SUMER <sumerisa5@gmail.com>
2021-09-10 13:24:58 +0300

    product list created

---
+++ ecommercial_website/src/pages/ProductList.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+
+const Container = styled.div`
+`;
+const ProductList = () => {
+    return (
+        <Container>
+            isa
+        </Container>
+    )
+}
+
+export default ProductList
